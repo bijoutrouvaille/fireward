79ee4a975874080576aecf2d05d0e814505d1153
Bijou Trouvaille <sometimes@bijou4u.irc>
2019-09-03 23:02:06 -0500

    travis windows backslash on tar

--- travis/buildfile.sh
+++ travis/buildfile.sh
@@ -2,7 +2,7 @@
 
 stack --no-terminal install --bench --no-run-benchmarks 
 if [ "$TRAVIS_OS_NAME" = "windows" ]; then
-  tar --force-local -cf $(stack path --local-bin)/${DEPLOY_FILE}.tar $(stack path --local-bin)/fireward$OSEXT 
+  tar --force-local -cf $(stack path --local-bin)\\${DEPLOY_FILE}.tar $(stack path --local-bin)\\fireward$OSEXT 
 else
   tar -cf $(stack path --local-bin)/${DEPLOY_FILE}.tar $(stack path --local-bin)/fireward$OSEXT 
 fi
