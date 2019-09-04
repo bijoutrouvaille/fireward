d05a6b163c157f4f609e81347a8b942cec0b5a3f
Bijou Trouvaille <sometimes@bijou4u.irc>
2019-09-03 22:20:14 -0500

    changed tar to zip due to travis windows bug

--- travis/buildfile.sh
+++ travis/buildfile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 stack --no-terminal install --bench --no-run-benchmarks 
-tar -cf $(stack path --local-bin)/${DEPLOY_FILE}.tar $(stack path --local-bin)/fireward$OSEXT 
+# tar -cf $(stack path --local-bin)/${DEPLOY_FILE}.tar $(stack path --local-bin)/fireward$OSEXT 
+zip $(stack path --local-bin)/${DEPLOY_FILE} $(stack path --local-bin)/fireward$OSEXT 
