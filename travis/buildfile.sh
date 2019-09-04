86afea0b3ee23742922ebca303096b7350bb23da
Bijou Trouvaille <sometimes@bijou4u.irc>
2019-09-03 22:47:29 -0500

    travis windows tar --force-local

--- travis/buildfile.sh
+++ travis/buildfile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 stack --no-terminal install --bench --no-run-benchmarks 
-# tar -cf $(stack path --local-bin)/${DEPLOY_FILE}.tar $(stack path --local-bin)/fireward$OSEXT 
-zip $(stack path --local-bin)/${DEPLOY_FILE} $(stack path --local-bin)/fireward$OSEXT 
+if [ "$TRAVIS_OS_NAME" = "windows" ]; then
+  tar --force-local -cf $(stack path --local-bin)/${DEPLOY_FILE}.tar $(stack path --local-bin)/fireward$OSEXT 
+else
+  tar -cf $(stack path --local-bin)/${DEPLOY_FILE}.tar $(stack path --local-bin)/fireward$OSEXT 
+fi
+# zip $(stack path --local-bin)/${DEPLOY_FILE} $(stack path --local-bin)/fireward$OSEXT 
