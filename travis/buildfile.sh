d18def0cbbd8c059b8d6ead5dd2b4b8a0d0af707
Bijou Trouvaille <sometimes@bijou4u.irc>
2019-09-03 23:42:35 -0500

    failing on copying file to its own location

--- travis/buildfile.sh
+++ travis/buildfile.sh
@@ -2,7 +2,9 @@
 
 stack --no-terminal install --bench --no-run-benchmarks 
 if [ "$TRAVIS_OS_NAME" = "windows" ]; then
-  cp "$(stack path --local-bin)\\fireward.exe" "$(stack path --local-bin)\\${DEPLOY_FILE}" 
+  # cp "$(stack path --local-bin)\\fireward.exe" "$(stack path --local-bin)\\${DEPLOY_FILE}" 
+  export DEPLOY_FILE=$(stack path --local-bin)\\fireward.exe
 else
-  cp "$(stack path --local-bin)\\fireward" "$(stack path --local-bin)\\${DEPLOY_FILE}"
+  export DEPLOY_FILE=$(stack path --local-bin)\\fireward-${TRAVIS_OS_NAME}
+  cp "$(stack path --local-bin)\\fireward" "$DEPLOY_FILE"
 fi
