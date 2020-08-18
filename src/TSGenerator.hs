cacf132b494b64762ebfb467d01d593b508e6079
Bijou Trouvaille <bijoutrouvaille@gmail.com>
2020-08-18 15:30:56 -0500

    WardTimestamp isEqual correction

--- src/TSGenerator.hs
+++ src/TSGenerator.hs
@@ -51,7 +51,7 @@ someTuple = "export type SomeTuple<T> = " ++ intercalate " | " [ "ArrayMax" ++ s
 funcMaxArray = ts ++ "\nexport function toArrayMax(n:number, arr:any[]) { return arr.slice(0,n) }" 
   where ts = intercalate "\n" ["export function toArrayMax<T>(n: "++show n++", arr:T[]):ArrayMax"++show n++"<T>" | n <- [1..maxTuples-1]]
 fieldValueType = "export type WardFieldValue = { isEqual: (other: WardFieldValue) => boolean };"
-timestampType = "export type WardTimestamp = {seconds: number, nanoseconds: number, toDate: ()=>Date, isEqual: (other: WardFieldValue)=>boolean, toMillis: ()=>number};"
+timestampType = "export type WardTimestamp = {seconds: number, nanoseconds: number, toDate: ()=>Date, isEqual: (other: WardTimestamp)=>boolean, toMillis: ()=>number};"
 timestampTypeCheck = "export function isTimestamp(v: any): v is WardTimestamp { return !!v && (typeof v=='object') && !!v.toDate && !!v.toMillis && (typeof v.nanoseconds=='number') && (typeof v.seconds=='number')};"
 geoPointType = "export type WardGeoPoint = { latitude: number, longitude: number, isEqual: (other: WardGeoPoint)=>boolean }"
 geoPointTypeCheck = "export function isGeoPoint(v: any): v is WardGeoPoint {  return !!v && (typeof v=='object') && (typeof v.isEqual=='function')  && (typeof v.latitude=='number') && (typeof v.longitude=='number') };"
